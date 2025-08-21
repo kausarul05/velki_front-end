84e02f9cdc4a7fe1557886691a375cb2c342fc2a
MD. Kauserul Islam <108425801+kausarul05@users.noreply.github.com>
2025-08-21 23:37:07 +0600

    fav icon and description add

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,8 +13,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "ভেলকি এজেন্ট লিস্ট | Velki Live Agent List 2025",
-  description: "সর্বশেষ ভেলকি এজেন্ট লিস্ট, Velki Live 123 Agent List, Winpbu Agent এবং Velki Master Agent List দেখতে এখানে ক্লিক করুন।",
+  title: "WINPBU তে স্বাগতম - Velki Baajiwala Agent List",
+  description: "WINPBU বাংলাদেশ - বাংলাদেশের শীর্ষস্থানীয় অনলাইন গেমিং এবং বেটিং প্ল্যাটফর্ম পরিচালনা প্রতিষ্টান। আমদের সাইট গুলো হচ্ছে এজেন্ট বেইজড সাইট। এই খানে এজেন্ট এর মাধ্যমে লেনদেন করতে হবে। এজেন্ট আপনাকে একাউন্ট খুলে দিবে।",
   keywords: [
     "ভেলকি এজেন্ট লিস্ট",
     "Velki Live Agent List 2025",
@@ -26,6 +26,24 @@ export const metadata: Metadata = {
     "ভেলকি লাইভ এজেন্ট লিস্ট",
     "Velki Bet Agent",
     "Velki Agent List Update",
+    "ভেলকি এজেন্ট লিস্ট",
+    "velki live login",
+    "velki live 123 agent list",
+    "velki live agent list",
+    "velki agent list",
+    "velki agent",
+    "Velki All Agent BD",
+    "Velki123 Live Agent List",
+    "velki agent list Canada",
+    "Velki Master Agent List",
+    "Bhajiwala agent list",
+    "winpbu agent list",
+    "winpbu agent",
+    "9wiket Agent list ",
+    "Bajiwola Agent list",
+    "ভেলকি এজেন্ট বাংলাদেশ",
+    "Winpbu Master Agent ",
+    "Winpbu Master",
   ],
 };
 
